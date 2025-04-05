f42aa6745ab48f9c67da14809818582c80dd1a75
HimanshuHeda <harsh967896@gmail.com>
2025-04-05 14:38:46 +0530

    Home & SignUp Design

--- app/src/main/java/com/example/earningapp/MainActivity.kt
+++ app/src/main/java/com/example/earningapp/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.earningapp.ui.theme.EarningAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EarningAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EarningAppTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
